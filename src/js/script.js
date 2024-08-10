30069281c9f97a946b1d48d911b1082b20380392
DanielVierh <daniel.vierheilig89@yahoo.de>
2024-08-10 20:51:15 +0200

    functions to discover cards

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,67 @@
+/**
+ * Ablauf:
+ * 
+ * Vor dem Spiel
+ * -Es wird der Stack gebildet. 
+ * -Dabei wird zu jeder Karte aus dem Objekt all_cards ein Objekt der Klasse Karte 
+ * erstellt.
+ * -Die erstellten Karten werden in ein Array namens cardStack gepusht
+ * -cardStack wird gemischt
+ * Es werden je 12 Karten verteilt. Erst für Spieler 1 dann für 2
+ * Der Index der Karten und des Stapels muss gleich sein
+ * 
+ * 
+ * Es werden zwei Spieler erstellt. Hierzu gibt es die Klasse Player.
+ * Spieler haben ein Cards Array
+ * Spieler haben eine Punkteanzahl, die nach jeder Runde erhöht wird
+ * Spieler haben einen index Wert (Player 1 bzw. 2)
+ * 
+ * 
+ * Rundenbasiert
+ * -Es beginnt immer Spieler 1 
+ * -Nach jeder Runde wird markiert, wer dran ist
+ * 
+ * Runde 0 
+ * -Es können zwei Karten umgedreht werden.
+ * -Zuerst fängt Spieler 1 an. Dann Spieler 2
+ * -Bei diesem Zug kann keine Karte aus dem Stack gezogen werden
+ * -Der Spieler mit der höheren Punktzahl beginnt
+ * 
+ * 
+ * 
+ * 
+ */
+
 const myBoard = document.getElementById('myBoard');
 const point_label = document.getElementById('point_label');
 const cards = document.querySelectorAll('.card');
 let player1;
 let player2;
+let cardStack = [];
+let ablageStack = [];
 
-const all_card_with_amount = {
+
+//*ANCHOR - Player Class
+
+class Player {
+    constructor(name, playerNumber) {
+        this.name = name;
+        this.cards = [];
+        this.points = 0;
+        this.firstRound = true;
+        this.playerNumber = playerNumber
+    }
+}
+
+class Card{
+    constructor(value, place, covered = false) {
+        this.value = value;
+        this.place = place;
+        this.covered = covered;
+    }
+}
+
+const all_cards = {
     '-2': 5,
     '-1': 10,
     '0': 15,
@@ -22,34 +79,27 @@ const all_card_with_amount = {
     '12': 10,
 };
 
-class Player {
-    constructor(name, playerNumber) {
-        this.name = name;
-        this.card = [];
-        this.points = 0;
-        this.firstRound = true;
-        this.playerNumber = playerNumber
-    }
-}
 
-let cardStack = [];
-let player1_cards = [];
 
 window.onload = init();
 
 function init() {
-    create_card_stack();
-    render_board();
-    //count_points();
     create_player();
+    create_card_stack();
+    give_player_cards(player1);
+    give_player_cards(player2);
+    //render_board();
+
+    //count_points();
 }
 
 
 //*ANCHOR - Create Stack
 function create_card_stack() {
-    for (let key in all_card_with_amount) {
-        for (let i = 0; i < all_card_with_amount[key]; i++) {
-            cardStack.push(key);
+    for (let key in all_cards) {
+        for (let i = 0; i < all_cards[key]; i++) {
+            const card = new Card(key, 'stack', true)
+            cardStack.push(card);
         }
     }
     cardStack = shuffleArray(cardStack)
@@ -65,70 +115,62 @@ function shuffleArray(array) {
     return array;
 }
 
-//*ANCHOR - Render 
-function render_board() {
-    let counter = 1;
-    //* Create cards for player 1
-    //TODO - Remove from cardStack
-    for (let i = 1; i < cardStack.length + 1; i++) {
-        if (counter <= 12) {
-            const card = document.getElementById(`player1_card_${i}`)
-            create_card(cardStack[i], card);
-            player1_cards.push(cardStack[i])
-            counter++;
-        } else {
-            break;
-        }
+//* Give 12 Cards to player
+function give_player_cards(_player) {
+    for (let i = 0; i < 12; i++) {
+        const card = cardStack.splice(i, 1);
+        _player.cards.push(card);
     }
 }
 
 
-function create_card(_card_value, _card) {
-    const data_status = _card.getAttribute('data-status');
-    if (data_status === 'covered') {
+//* 
+function create_card(_card, _cardSlot) {
+    const data_status = _card.covered;
+    if (data_status === false) {
         return
     } else {
-        _card.classList.remove('covered')
+        document.getElementById(_cardSlot).classList.remove('covered')
     }
 
     let vallabel = document.createElement('p');
     let before_label = document.createElement('p');
     let after_label = document.createElement('p');
-    before_label.innerHTML = _card_value;
+    before_label.innerHTML = _card.value;
     before_label.classList.add('before-label');
     after_label.classList.add('after-label');
-    if (_card_value == 6 || _card_value == 9) {
+    if (_card.value == 6 || _card.value == 9) {
         vallabel.classList.add('underlined')
         before_label.classList.add('underlined')
         after_label.classList.add('underlined')
     }
-    after_label.innerHTML = _card_value;
-    vallabel.innerHTML = _card_value;
+    after_label.innerHTML = _card.value;
+    vallabel.innerHTML = _card.value;
     vallabel.classList.add('val-label')
-    _card.appendChild(vallabel);
-    _card.appendChild(before_label);
-    _card.appendChild(after_label);
-    _card.classList.add('card');
-    if (_card_value > 0 && _card_value < 5) {
-        _card.classList.add('green');
-    } else if (_card_value >= 5 && _card_value < 9) {
-        _card.classList.add('yellow');
-    } else if (_card_value >= 9 && _card_value <= 12) {
-        _card.classList.add('red');
-    } else if (_card_value == 0) {
-        _card.classList.add('lightblue');
-    } else if (_card_value == -1) {
-        _card.classList.add('blue');
-    } else if (_card_value == -2) {
-        _card.classList.add('blue');
+    document.getElementById(_cardSlot).appendChild(vallabel);
+    document.getElementById(_cardSlot).appendChild(before_label);
+    document.getElementById(_cardSlot).appendChild(after_label);
+    document.getElementById(_cardSlot).classList.add('card');
+    if (_card.value > 0 && _card.value < 5) {
+        document.getElementById(_cardSlot).classList.add('green');
+    } else if (_card.value >= 5 && _card.value < 9) {
+        document.getElementById(_cardSlot).classList.add('yellow');
+    } else if (_card.value >= 9 && _card.value <= 12) {
+        document.getElementById(_cardSlot).classList.add('red');
+    } else if (_card.value == 0) {
+        document.getElementById(_cardSlot).classList.add('lightblue');
+    } else if (_card.value == -1) {
+        document.getElementById(_cardSlot).classList.add('blue');
+    } else if (_card.value == -2) {
+        document.getElementById(_cardSlot).classList.add('blue');
     }
 }
 
 function count_points() {
     let points_player1 = 0;
 
-    for (let i = 0; i < player1_cards.length; i++) {
-        points_player1 = points_player1 += parseInt(player1_cards[i]);
+    for (let i = 0; i < player1.cards.length; i++) {
+        points_player1 = points_player1 += parseInt(player1.cards[i]);
     }
 
     point_label.innerHTML = `${points_player1} Punkte`;
@@ -139,9 +181,21 @@ function count_points() {
 
 cards.forEach((card) => {
     card.addEventListener('click', () => {
-        console.log(card);
+        const card_slot_id = card.id;
+        const card_index = card.getAttribute("data-index");
+        const player = card.getAttribute("data-player");
+        let player_card
+
+        if(player === 'player1') {
+             player_card = player1.cards[card_index][0];
+        }else {
+            player_card = player2.cards[card_index][0];
+        }
+        create_card(player_card, card_slot_id)
     })
 })
+
+//*ANCHOR - Create Player 
 
 function create_player() {
     for (let i = 1; i <= 2; i++) {
@@ -158,3 +212,4 @@ function create_player() {
     console.log(player1);
     console.log(player2);
 }
+
